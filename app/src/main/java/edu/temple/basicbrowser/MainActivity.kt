75a362bd6586fd0f5eab87a1d0bf80ec58168c36
JasonC136 <tun38667@temple.edu>
2022-10-27 12:18:32 -0400

    Added all the above

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -3,26 +3,40 @@ package edu.temple.basicbrowser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var urlEditText: EditText
-    lateinit var goButton: Button
+    lateinit var goButton: ImageButton
     lateinit var webView: WebView
+    lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         urlEditText = findViewById(R.id.urlEditText)
-        goButton = findViewById(R.id.goButton)
+        goButton = findViewById(R.id.ImageButton)
         webView = findViewById(R.id.webView)
 
-        // Allow your browser to intercept hyperlink clicks
-        webView.webViewClient = WebViewClient()
 
+        requestQueue = Volley.newRequestQueue(this)
+
+        webView.settings.javaScriptEnabled = true
+
+        goButton.setOnClickListener {
+            val userInput = urlEditText.text
+            requestQueue.add(
+                StringRequest(Request.Method.GET, "$userInput", {
+                    webView.loadDataWithBaseURL("", it, "text/html", "utf-8", null)
+                }, {})
+            )
+        }
     }
 }
